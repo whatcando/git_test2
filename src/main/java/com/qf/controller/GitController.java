f72d8bf819d437f30a1cba77536bf0ba20178a2f
wwg <2075676902@qq.com>
2019-09-26 16:17:06 +0800

    firest commit wwg

---
+++ src/main/java/com/qf/controller/GitController.java
@@ -0,0 +1,14 @@
+package com.qf.controller;
+
+import java.sql.SQLOutput;
+
+/**
+ * @Author wangwengang
+ * @Date 2019/9/26
+ */
+public class GitController {
+    public static void main(String[] args) {
+        System.out.println("adfa");
+        System.out.println("adfadfaf");
+    }
+}
